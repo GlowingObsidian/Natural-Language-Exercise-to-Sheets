b8779bca196d56d40cb36366987cb849aa73fece
Shreyan Dey <deyshreyan24@outlook.com>
2023-08-26 23:02:08 +0530

    add process input and get exercise data

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ API_KEY = os.getenv("API_KEY")
 
 natural_lang_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
-params = {
-    "query": "I ran for 3 Km"
-}
-
 headers = {
     "x-app-id": APP_ID,
     "x-app-key": API_KEY,
+}
+
+query = input("Tell me which exercises you did: ")
+
+params = {
+    "query": query
 }
 
 data = requests.post(url=natural_lang_endpoint, json=params, headers=headers)
